765c5222302d8c757cd728f648b09263adcfd394
Djordje Dragicevic <djordje.dragicevic@yahoo.com>
2022-11-08 14:16:56 +0100

    API remove tap-api project

--- tap-api/src/main/java/com/tap/rest/TAPApplication.java
+++
@@ -1,9 +0,0 @@
-package com.tap.rest;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class TAPApplication extends Application {
-
-}
