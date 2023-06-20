ec5fbda5c00f82f9ec26aee382dc03a71c27c6fd
Djordje Dragicevic <djordje.dragicevic@yahoo.com>
2023-06-20 12:20:20 +0200

    API - Bump versions of microprofile and persistence to 6.0 and 3.1

--- tap-api/src/main/java/com/tap/TAPApplication.java
+++ tap-api/src/main/java/com/tap/TAPApplication.java
@@ -3,7 +3,7 @@ package com.tap;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/api")
+@ApplicationPath("/api/v1")
 public class TAPApplication extends Application {
 
 }
--- tap-api/src/main/java/com/tap/provider/ProviderDAO.java
+++ tap-api/src/main/java/com/tap/provider/ProviderDAO.java
@@ -4,7 +4,9 @@ import com.tap.db.entity.Provider;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Tuple;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RequestScoped
@@ -13,6 +15,12 @@ public class ProviderDAO {
 	private EntityManager em;
 
 	public List getProviders() {
-		return em.createQuery("SELECT p FROM Provider p", Provider.class).getResultList();
+		String qS = """
+				SELECT p.id, p.address.address1 FROM Provider p
+				""";
+
+		List<Tuple> list = em.createQuery(qS, Tuple.class).getResultList();
+
+		return list;
 	}
 }
